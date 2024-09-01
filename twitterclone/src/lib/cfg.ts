1314faf5c8f46e2c45379d102e7187e77ce9de30
Simen Wiik <simenwiik@hotmail.com>
2024-09-01 16:35:38 +0200

    getPost... and getUser... in lib/cfg

--- twitterclone/src/lib/cfg.ts
+++ twitterclone/src/lib/cfg.ts
@@ -3,9 +3,9 @@ import {getEnvironment} from "wasi:cli/environment@0.2.0";
 let env: Map<string, string> | undefined = undefined;
 
 export const envVarKeys = {
-    COMPONENT_ONE_ID: "COMPONENT_ONE_ID",
-    COMPONENT_TWO_ID: "COMPONENT_TWO_ID",
-    COMPONENT_THREE_ID: "COMPONENT_THREE_ID",
+    USER_COMPONENT_ID: "urn:component:c09bde19-80f5-4786-b863-24be77ad7deb",
+    POST_COMPONENT_ID: "urn:component:f2859f2f-919e-43b0-b89d-3aa36a3ab172",
+    // COMPONENT_THREE_ID: "COMPONENT_THREE_ID",
 }
 
 function getEnv(key: string): string | undefined {
@@ -35,14 +35,14 @@ function getComponentWorkerURN(componentID: string, workerName: string): string 
     return `urn:worker:${componentID}/${workerName}`;
 }
 
-export function getComponentOneWorkerURN(workerName: string): Uri {
-    return {value: getComponentWorkerURN(mustGetEnv(envVarKeys.COMPONENT_ONE_ID), workerName)};
+export function getUserWorkerURN(workerName: string): Uri {
+    return {value: getComponentWorkerURN(mustGetEnv(envVarKeys.USER_COMPONENT_ID), workerName)};
 }
 
-export function getComponentTwoWorkerURN(workerName: string): Uri {
-    return {value: getComponentWorkerURN(mustGetEnv(envVarKeys.COMPONENT_TWO_ID), workerName)};
+export function getPostWorkerURN(workerName: string): Uri {
+    return {value: getComponentWorkerURN(mustGetEnv(envVarKeys.POST_COMPONENT_ID), workerName)};
 }
 
-export function getComponentThreeWorkerURN(workerName: string): Uri {
-    return {value: getComponentWorkerURN(mustGetEnv(envVarKeys.COMPONENT_THREE_ID), workerName)};
-}
+// export function getComponentThreeWorkerURN(workerName: string): Uri {
+//     return {value: getComponentWorkerURN(mustGetEnv(envVarKeys.COMPONENT_THREE_ID), workerName)};
+// }
